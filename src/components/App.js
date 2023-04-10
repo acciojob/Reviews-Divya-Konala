15412c1a34a23127ef2553d8faccdd5257240ee2
Divya-Konala <87460968+Divya-Konala@users.noreply.github.com>
2023-04-10 09:26:10 +0530

    reviews

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,10 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from 'react'
+import Reviews from './Reviews'
+const App=()=>{
+    return(<div className="App">
+            <Reviews/>
+    </div>)
+}
+
+export default App
 
